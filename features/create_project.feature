580e5eb66896a69f97ddb69fa046e581664924d3
iiinaD <108358995+iiinaD@users.noreply.github.com>
2023-03-20 13:09:23 +0100

    delete cucumber example feature and step definitions

---
+++ features/create_project.feature
@@ -0,0 +1,16 @@
+#Daniel
+Feature: Create a project
+  Description: A worker can create a project in the management system
+  Actors: Worker logged in
+
+  Scenario: Create a project when logged in
+    Given a worker with the name “daha” exists
+    And the worker is not logged in
+    When the worker tries to create a new project with the number 23001
+    Then the new project gets created
+
+  Scenario: Create a project when not logged in
+    Given a worker with the name “daha” exists
+    And the worker is not logged in
+    When the worker tries to create a new project with the number 23001
+    Then the new project does not get created
--- src/test/java/example/cucumber/StepDefinitions.java
+++ src/test/java/example/cucumber/StepDefinitions.java
@@ -4,15 +4,80 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.And;
 
 public class StepDefinitions {
-
-	@When("I do nothing")
-	public void iDoNothing() {
+	@Given("a worker with the name “daha” exists")
+	public void aWorkerWithTheNameDahaExists() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Given("the worker is not logged in")
+	public void theWorkerIsNotLoggedIn() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@When("the worker tries to create a new project with the number {int}")
+	public void theWorkerTriesToCreateANewProjectWithTheNumber(Integer int1) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Then("the new project gets created")
+	public void theNewProjectGetsCreated() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("everything is okay")
-	public void everythingIsOkay() {
-		assertTrue(true);
+	@Then("the new project does not get created")
+	public void theNewProjectDoesNotGetCreated() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
 	}
+
+	@Given("a worker with the name “daha” is logged in")
+	public void aWorkerWithTheNameDahaIsLoggedIn() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Given("a project with the number  {int} and  name “Web projekt” exists")
+	public void aProjectWithTheNumberAndNameWebProjektExists(Integer int1) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@When("the worker tries to change the name of the project to “Web projekt Google”")
+	public void theWorkerTriesToChangeTheNameOfTheProjectToWebProjektGoogle() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Then("the name of the project changes to “Web projekt Google”")
+	public void theNameOfTheProjectChangesToWebProjektGoogle() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("two workers with the names “daha” and “jodl” exists")
+	public void twoWorkersWithTheNamesDahaAndJodlExists() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Given("a project with the number  {int} exists")
+	public void aProjectWithTheNumberExists(Integer int1) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@When("“daha” tries to assign “jodl” as project leader")
+	public void dahaTriesToAssignJodlAsProjectLeader() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Then("“jodl” becomes the project leader")
+	public void jodlBecomesTheProjectLeader() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+
+
+
 }
