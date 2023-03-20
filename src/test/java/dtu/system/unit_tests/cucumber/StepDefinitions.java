a7d72efdfe5f36c303e2090eb83dd1a73f42f0a0
iiinaD <108358995+iiinaD@users.noreply.github.com>
2023-03-20 16:57:23 +0100

    final fix to the stepdefinitions for create and edit project

--- src/test/java/dtu/system/unit_tests/cucumber/StepDefinitions.java
+++ src/test/java/dtu/system/unit_tests/cucumber/StepDefinitions.java
@@ -18,24 +18,43 @@ public class StepDefinitions {
 		this.app = app;
 		//this.errorMessageHolder = errorMessageHolder;
 	}
-
+	@Given("a worker with the name {string} exists")
+	public void aWorkerWithTheNameExists(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
 	@Given("a worker with the name {string} is logged in")
-	public void a_worker_with_the_name_is_logged_in(String string) {
+	public void aWorkerWithTheNameIsLoggedIn(String string) {
 		// Write code here that turns the phrase above into concrete actions
 		throw new io.cucumber.java.PendingException();
 	}
 	@Given("a project with the number  {int} and  name {string} exists")
-	public void a_project_with_the_number_and_name_exists(Integer int1, String string) {
+	public void aProjectWithTheNumberAndNameExists(Integer int1, String string) {
 		// Write code here that turns the phrase above into concrete actions
 		throw new io.cucumber.java.PendingException();
 	}
 	@When("the worker tries to change the name of the project to {string}")
-	public void the_worker_tries_to_change_the_name_of_the_project_to(String string) {
+	public void theWorkerTriesToChangeTheNameOfTheProjectTo(String string) {
 		// Write code here that turns the phrase above into concrete actions
 		throw new io.cucumber.java.PendingException();
 	}
 	@Then("the name of the project changes to {string}")
-	public void the_name_of_the_project_changes_to(String string) {
+	public void theNameOfTheProjectChangesTo(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Given("two workers with the names {string} and {string} exists")
+	public void twoWorkersWithTheNamesAndExists(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@When("{string} tries to assign {string} as project leader")
+	public void triesToAssignAsProjectLeader(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Then("{string} becomes the project leader")
+	public void becomesTheProjectLeader(String string) {
 		// Write code here that turns the phrase above into concrete actions
 		throw new io.cucumber.java.PendingException();
 	}
@@ -65,21 +84,6 @@ public class StepDefinitions {
 		throw new io.cucumber.java.PendingException();
 	}
 
-	@Given("two workers with the names {string} and {string} exists")
-	public void two_workers_with_the_names_and_exists(String string, String string2) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-	@When("{string} tries to assign {string} as project leader")
-	public void tries_to_assign_as_project_leader(String string, String string2) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-	@Then("{string} becomes the project leader")
-	public void becomes_the_project_leader(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
 
 	@Given("a project with the number  {int} exists")
 	public void aProjectWithTheNumberExists(Integer int1) {
