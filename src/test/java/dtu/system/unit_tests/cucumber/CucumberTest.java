0af08772a52148f30fe2bfbec2c6a6364cd09db5
Jonas <jonas.larsen103@gmail.com>
2023-03-27 15:28:41 +0200

    test commit

--- src/test/java/dtu/system/unit_tests/cucumber/CucumberTest.java
+++ src/test/java/dtu/system/unit_tests/cucumber/CucumberTest.java
@@ -9,6 +9,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 /* Important:
 test
+new test
 for Cucumber tests to be recognized by Maven, the class name has to have
 either the word Test in the beginning or at the end. 
 For example, the class name CucumberTests (Test with an s) will be ignored by Maven.
