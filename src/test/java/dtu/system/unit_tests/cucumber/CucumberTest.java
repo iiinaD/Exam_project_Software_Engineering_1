5dc982c1316a7cea45bac574e7be516cbd8bd844
Jonas <jonas.larsen103@gmail.com>
2023-03-27 15:22:33 +0200

    trying to push

--- src/test/java/dtu/system/unit_tests/cucumber/CucumberTest.java
+++ src/test/java/dtu/system/unit_tests/cucumber/CucumberTest.java
@@ -7,7 +7,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 
-/* Important: 
+/* Important:
+test
 for Cucumber tests to be recognized by Maven, the class name has to have
 either the word Test in the beginning or at the end. 
 For example, the class name CucumberTests (Test with an s) will be ignored by Maven.
