a36fd2ea3ada3f28b5ef74c8a2aa1fed92b09def
iiinaD <108358995+iiinaD@users.noreply.github.com>
2023-04-13 12:21:01 +0200

    begin implementation of new ui

--- src/main/java/dtu/system/UI/userInterface.java
+++ src/main/java/dtu/system/UI/userInterface.java
@@ -1,52 +1,61 @@
 package dtu.system.UI;
 
 import dtu.system.app.Application;
-import dtu.system.commands.*;
 
-import java.awt.desktop.AppForegroundListener;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class userInterface {
-
-    public static void main(String[] args){
-        Map<String, Command> commands = new HashMap<>();
-        loadCommands(commands);
-
-        Scanner console = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Daniel
         Application app = new Application();
-
-        System.out.println("System has started.");
-        System.out.println("For list of commands type \"help\"");
-
-        while (true) {
-            System.out.print("Please enter a command:\n > ");
-            String input = console.nextLine();
-            Command command = commands.get(input);
-
-            if (exitTest(input)) {
-                break;
-            }
-
-            if (command != null) {
-                command.execute(app);
-            } else {
-                System.out.println("Unknown command for list of valid commands type \"help\"");
+        Scanner terminal = new Scanner(System.in);
+        boolean isExitChosen = false;
+        while (!isExitChosen) {
+            printLoginMenu();
+            System.out.println("Please choose an item from the menu list");
+            int input = getIntInput(terminal,2,"The number has to correspond to one of the menu items.");
+            if (input == 1) {
+                String initials = terminal.next();
+                app.logIn(initials);
+                // Load next menu
+            } else if (input == 2) {
+                isExitChosen = true;
             }
         }
-
-
-        console.close();
+        System.out.println("Program terminated.");
+    }
+    public static int getIntInput(Scanner terminal,int numberOfMenuItems,String message) {
+        // Check for a valid number
+        while(!terminal.hasNextInt()) {
+            System.out.println(message);
+            System.out.println();
+        }
+        return terminal.nextInt();
+    }
+    public static void printLoginMenu() {
+        System.out.println("1. Login");
+        System.out.println("2. Exit program");
+        System.out.println();
+    }
+    public static void printMainMenu() {
+        System.out.println("1. Add a worker");
+        System.out.println("2. Create a project");
+        System.out.println("3. Access a project");
+        System.out.println("4. Register working hours");
+        System.out.println("5. Logout");
+        System.out.println();
     }
 
-    public static boolean exitTest(String input){
-        return (input.equalsIgnoreCase("Exit") || input.equalsIgnoreCase("Quit"));
+    public static void printProjectMenu() {
+        System.out.println("1. Edit project");
+        System.out.println("2. Add an activity");
+        System.out.println("3. Access an acivity");
+        System.out.println();
     }
 
-    private static void loadCommands(Map<String, Command> commands) {
-        commands.put("login", (Command) new Login());
-        commands.put("create project", (Command) new CreateProject());
-        commands.put("add worker", (Command) new AddWorker());
+    public static void printActivityMenu() {
+
     }
 }
--- src/main/java/dtu/system/commands/Command.java
+++
@@ -1,7 +0,0 @@
-package dtu.system.commands;
-
-import dtu.system.app.Application;
-
-public interface Command {
-    void execute(Application app);
-}
