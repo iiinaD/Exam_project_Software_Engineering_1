a8e3a07ab2766d53c35f5c3cf3cba9a184712182
Jonas Larsen <jonas.larsen103@gmail.com>
2023-05-01 13:19:27 +0200

    stuf

--- src/test/java/dtu/system/unit_tests/junit/SystematicTests.java
+++ src/test/java/dtu/system/unit_tests/junit/SystematicTests.java
@@ -1,5 +1,10 @@
 package dtu.system.unit_tests.junit;
-
+import dtu.system.domain.HalfHours;
 public class SystematicTests {
-    
+
+    @org.junit.Test
+    public void junit1Test() {
+        // Jonas
+        HalfHours halfHours = new HalfHours(0,0);
+    }
 }
