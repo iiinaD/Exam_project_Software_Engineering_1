22d31c18a3f428ab8b049d53d5197e8169ae37ed
Man-Gee <contact@yeungmang.ee>
2023-05-01 15:05:41 +0200

    add test

--- src/test/java/dtu/system/unit_tests/junit/SystematicTests.java
+++ src/test/java/dtu/system/unit_tests/junit/SystematicTests.java
@@ -1,8 +1,10 @@
 package dtu.system.unit_tests.junit;
+import dtu.system.app.*;
 import static org.junit.jupiter.api.Assertions.*;
 import dtu.system.domain.HalfHours;
 
 public class SystematicTests {
+    private Application app = new Application();
     @org.junit.Before // Junit 4
     public void setUp() {
 
@@ -43,5 +45,42 @@ public class SystematicTests {
         F.increment(0, -30);
         assertEquals(0, F.getTime());
     }
+
+    //validProjectNumberTest test
+    //adapted from ChatGPT response
+    @org.junit.Test
+    public void testValidProjectNumber() throws OperationNotAllowedException {
+        // Arrange
+        int number = 23001; // valid project number
+
+        // Act
+        boolean result = app.validProjectNumberTest(number);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @org.junit.Test
+    public void testInvalidProjectNumberTooSmall() {
+        // Arrange
+        int number = 2300; // invalid project number, too small
+
+        // Act & Assert
+        assertThrows(OperationNotAllowedException.class, () -> {
+            app.validProjectNumberTest(number);
+        });
+    }
+
+    @org.junit.Test
+    public void testInvalidProjectNumberTooLarge() {
+        // Arrange
+        int number = 230001; // invalid project number, too large
+
+        // Act & Assert
+        assertThrows(OperationNotAllowedException.class, () -> {
+            app.validProjectNumberTest(number);
+        });
+    }
+
 
 }
