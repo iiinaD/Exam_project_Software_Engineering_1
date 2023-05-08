a1a59f6c390e1ba5191481890bceeb922fa890a3
DBorke <dannybborke@gmail.com>
2023-05-08 11:25:09 +0200

    Added authors and removed unused imports.

--- src/test/java/dtu/system/unit_tests/cucumber/CucumberTest.java
+++ src/test/java/dtu/system/unit_tests/cucumber/CucumberTest.java
@@ -1,6 +1,5 @@
 package dtu.system.unit_tests.cucumber;
 
-
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
--- src/test/java/dtu/system/unit_tests/cucumber/ErrorMessageHolder.java
+++ src/test/java/dtu/system/unit_tests/cucumber/ErrorMessageHolder.java
@@ -1,15 +1,15 @@
 package dtu.system.unit_tests.cucumber;
 
+// Danny
+
 public class ErrorMessageHolder {
      private String errorMessage;
 
      public String getErrorMessage() {
-          // Danny
           return errorMessage;
      }
 
      public void setErrorMessage(String errorMessage) {
-          // Danny
           this.errorMessage = errorMessage;
      }
 }
